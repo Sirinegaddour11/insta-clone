0e9b23b2e59aae92fede92e470e22b2a3863c778
syrina gaddour <syrinagaddour11@gmail.com>
2022-09-23 16:20:40 +0100

    update Feed

--- components/Feed/Feed.js
+++ components/Feed/Feed.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Posts from "./Posts/Posts";
 import Stories from "./Stories/Stories";
 
 function Feed() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
       <section  className="col-span-2">
         {/* Stories  */}
         <Stories />
 
         {/* Posts  */}
+        <Posts />
       </section>
 
       <section>
