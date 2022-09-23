47b11887522e86f5bbd7758be757c0fd42af9933
syrina gaddour <syrinagaddour11@gmail.com>
2022-09-23 17:31:57 +0100

    Update Feed

--- components/Feed/Feed.js
+++ components/Feed/Feed.js
@@ -1,11 +1,13 @@
 import React from "react";
+import MiniProfile from "./MiniProfile/MiniProfile";
 import Posts from "./Posts/Posts";
 import Stories from "./Stories/Stories";
+import Suggestions from "./Suggestions/Suggestions";
 
 function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
-      <section  className="col-span-2">
+      <section className="col-span-2">
         {/* Stories  */}
         <Stories />
 
@@ -13,10 +15,16 @@ function Feed() {
         <Posts />
       </section>
 
-      <section>
+      <section className="hidden xl:inline-grid md:col-span-1">
+        <div className="fixed">
         {/* Mini profile  */}
+        <MiniProfile />
+
         {/* Suggestions  */}
+        <Suggestions />
+        </div>
       </section>
+
     </main>
   );
 }
