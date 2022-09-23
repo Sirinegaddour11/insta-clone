ac1501c443d0da15450fe11f6212a9aa1599ca9b
syrina gaddour <syrinagaddour11@gmail.com>
2022-09-23 11:59:57 +0100

    update Feed

--- components/Feed/Feed.js
+++ components/Feed/Feed.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Stories from "./Stories/Stories";
 
 function Feed() {
   return (
-    <div>
-      
-    </div>
-  )
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
+      <section  className="col-span-2">
+        {/* Stories  */}
+        <Stories />
+
+        {/* Posts  */}
+      </section>
+
+      <section>
+        {/* Mini profile  */}
+        {/* Suggestions  */}
+      </section>
+    </main>
+  );
 }
 
-export default Feed
+export default Feed;
