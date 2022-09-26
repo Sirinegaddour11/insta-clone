e451f0fb94de4861e7404be0ecc4e0dbb7750efc
syrina gaddour <syrinagaddour11@gmail.com>
2022-09-26 14:23:36 +0100

    update

--- components/Feed/Stories/Stories.js
+++ components/Feed/Stories/Stories.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Story from "./Story";
 function Stories() {
   const [suggestions, setSuggestions] = useState([]);
+  // const [te, settes] = useState(()=>{});
+  // const [tte, settt] = useState(rev=>{});
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
       userId: faker.datatype.uuid(),
@@ -15,12 +17,13 @@ function Stories() {
       registeredAt: faker.date.past(),
     }));
     setSuggestions(suggestions);
-  
   }, []);
 
   return (
-    <div className="flex space-x-2 p-6 bg-white mt-8
-    border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+    <div
+      className="flex space-x-2 p-6 bg-white mt-8
+    border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black"
+    >
       {suggestions.map((profile) => (
         <Story
           key={profile.userId}
